cb9a6704af72243b159af255dcc11e39bd1036af
fadysaadeddin <fadysaadeddin@gmail.com>
2024-10-22 04:14:15 +0200

    added 10 question

--- src/data.js
+++ src/data.js
@@ -11,50 +11,219 @@
 
 export const quizData = {
   currentQuestionIndex: 0,
-  // the questions in the quiz
+  
   questions: [
     {
-      text: 'What are the different ways to declare a JS variable?',
+      text: 'What is the capital of Spain?',
       answers: {
-        a: 'constant, let, variable',
-        b: 'var, const, let, function',
-        c: 'var, let, const',
+        a: 'Berlin',
+        b: 'Rome',
+        c: 'Paris',
+        d: 'Madrid'
       },
-      correct: 'c',
+      correct: 'd',
       selected: null,
       links: [
         {
-          text: 'javascript.info',
-          href: 'https://javascript.info/variables',
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
         },
         {
-          text: 'Tyler McGinnis',
-          href: 'https://ui.dev/var-let-const/',
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
         },
       ],
     },
     {
-      text: 'What does `typeof` do?',
+      text: 'What is the largest ocean on earth?',
       answers: {
-        a: 'changes the type of a primitive value',
-        b: 'returns a string describing the type of a value',
-        c: 'determines if a value is primitive',
-        d: 'can tell the difference between arrays and objects',
+        a: 'Indian Ocean',
+        b: 'Pacific Ocean',
+        c: 'Atlantic Ocean',
+        d: 'Arctic Ocean',
       },
       correct: 'b',
       selected: null,
       links: [
         {
-          text: 'javascript.info',
-          href: 'https://javascript.info/types#type-typeof',
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
         },
         {
-          text: 'MDN',
+          text: 'Geography',
           href:
-            'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
+            'https://www.wikipedia.org/',
         },
       ],
     },
-    // Add more questions here
+    {
+      text: 'How many oceans do we have on earth?',
+      answers: {
+        a: 'two',
+        b: 'three',
+        c: 'one',
+        d: 'five'
+      },
+      correct: 'd',
+      selected: null,
+      links: [
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+      ],
+    },
+    {
+      text: 'In which continent is Brazil located?',
+      answers: {
+        a: 'North America',
+        b: 'Africa',
+        c: 'South America',
+        d: 'Asia'
+      },
+      correct: 'd',
+      selected: null,
+      links: [
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+      ],
+    },
+    {
+      text: 'What is the official language of Syria?',
+      answers: {
+        a: 'Arabic',
+        b: 'English',
+        c: 'French',
+        d: 'Dutch'
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+      ],
+    },
+    {
+      text: 'Which of these is the highest mountain in the world?',
+      answers: {
+        a: 'Denali',
+        b: 'Matterhorn',
+        c: 'Everest',
+        d: 'fuji'
+      },
+      correct: 'c',
+      selected: null,
+      links: [
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+      ],
+    },
+    {
+      text: 'What is the official religion of Egypt ?',
+      answers: {
+        a: 'Islam',
+        b: 'Christianity',
+        c: 'buddhism',
+        d: 'hinduism'
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+      ],
+    },
+    {
+      text: 'What is the biggest country in the world?',
+      answers: {
+        a: 'Brazil',
+        b: 'Russia',
+        c: 'Canada',
+        d: 'china'
+      },
+      correct: 'b',
+      selected: null,
+      links: [
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+      ],
+    },
+    {
+      text: 'What is the longest river in the world?',
+      answers: {
+        a: 'Nile',
+        b: 'Amazon',
+        c: 'Mississippi',
+        d: 'Danube'
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+      ],
+    },
+    {
+      text: 'which country shares the eastern border of the Netherlands?',
+      answers: {
+        a: 'Germany',
+        b: 'Spain',
+        c: 'Italy',
+        d: 'France'
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+        {
+          text: 'Geography',
+          href: 'https://www.wikipedia.org/',
+        },
+      ],
+    },
+    
   ],
 };
