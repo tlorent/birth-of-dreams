2326bd6a4b8012a32556890d86a0c77e04c40dbf
Dalia-Saeed <dalisaeed09@gmail.com>
2024-10-23 11:41:31 +0200

    added the title and favicon in app.js

--- src/app.js
+++ src/app.js
@@ -8,3 +8,14 @@ const loadApp = () => {
 };
 
 window.addEventListener('load', loadApp);
+
+// to change the title
+document.title = "Geography App";
+// to make the favicon
+const link = document.createElement("link");
+link.rel = "icon";
+link.href = "https://cdn-icons-png.flaticon.com/128/4523/4523524.png";
+document.head.appendChild(link);
+// to do the score counter
+
+
